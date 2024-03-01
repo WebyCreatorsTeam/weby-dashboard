d5575d554a8f09468355694eef8893a274244a59
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-03-01 21:31:34 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const cors = require('cors')
 app.use(express.json());
 app.use(cookieParser());
 
-const whitelist = [""]
+const whitelist = [process.env.CORS]
 const corsOptions = {
     origin: function (origin, callback) {
         if (whitelist.indexOf(origin) !== -1 || !origin) {
